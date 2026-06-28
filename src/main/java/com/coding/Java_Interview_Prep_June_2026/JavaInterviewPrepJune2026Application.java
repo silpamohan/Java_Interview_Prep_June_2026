77c65cfca64581224116216f4e1211e34d8c0f8d
Silpa Mohan <silpamohan@users.noreply.github.com>
2026-06-28 22:11:53 +0530

    Initial commit

---
+++ src/main/java/com/coding/Java_Interview_Prep_June_2026/JavaInterviewPrepJune2026Application.java
@@ -0,0 +1,13 @@
+package com.coding.Java_Interview_Prep_June_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInterviewPrepJune2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInterviewPrepJune2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding/Java_Interview_Prep_June_2026/JavaInterviewPrepJune2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.Java_Interview_Prep_June_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInterviewPrepJune2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
